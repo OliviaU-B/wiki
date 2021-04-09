35ba0dc4950ccabfed0f57f309d360f218409bfd
OliviaU-B <olivia.u-b@hotmail.com>
2021-04-09 11:48:37 +0100

    Add url patterns

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<title>", views.get_entry, name="get_entry"),
+    path("search", views.search, name="search"),
+    path("random", views.random_search, name="random"),
+    path("create-entry", views.create_entry, name="create_entry"),
+    path("edit/<title>", views.edit, name="edit"),
 ]
